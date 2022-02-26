af3f60e8eb8bea6dd67a58b3fa5fad098671d38d
Jean-Poul <cph-jl360@cphbusiness.dk>
2022-02-26 13:05:38 +0100

    entity addons and date function

--- assignment/src/main/java/cph/databases/assignment/entity/Pharmacy.java
+++ assignment/src/main/java/cph/databases/assignment/entity/Pharmacy.java
@@ -1,5 +1,7 @@
 package cph.databases.assignment.entity;
 
+import com.sun.istack.NotNull;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -13,7 +15,50 @@ public class Pharmacy {
     private Long id;
     @OneToMany(mappedBy = "pharmacy", fetch = FetchType.EAGER)
     private Collection<Handout> handoutList = new ArrayList<>();
+    @NotNull
+    private String addresse;
+    @NotNull
+    private int phone;
 
+    // Constructors
     public Pharmacy() {
+    }
+
+    public Pharmacy(String addresse, int phone) {
+        this.addresse = addresse;
+        this.phone = phone;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Collection<Handout> getHandoutList() {
+        return handoutList;
+    }
+
+    public void setHandoutList(Collection<Handout> handoutList) {
+        this.handoutList = handoutList;
+    }
+
+    public String getAddresse() {
+        return addresse;
+    }
+
+    public void setAddresse(String addresse) {
+        this.addresse = addresse;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
     }
 }
